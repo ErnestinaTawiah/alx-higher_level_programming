dd9b3db6a6c954cd8fc7ab8be72acee6d94aa85c
ErnestinaTawiah <starkles1181@gmail.com>
2022-06-03 16:41:26 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv_count = len(argv)
     index = 1
     res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+while index <= argv_count:
+res += int(sys.argv[index])
+index += 1
+print("{:d}".format(res))
